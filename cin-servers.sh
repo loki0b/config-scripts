aa79bdb6ae4bd4334c83251f1eb11c616e0d365e
loki0b <riqc0nt4ct@hotmail.com>
2024-03-02 17:19:35 -0300

    remove unused scripts

--- cin-servers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S smbclient gvfs-smb --needed
-
-gio mount "smb://webserver.cin.ufpe.br/$1"
-
-gio mount "smb://virtualdisk.cin.ufpe.br/$1"
-
--- config.sh
+++
@@ -1,1 +0,0 @@
-# config pacman mirror and color
