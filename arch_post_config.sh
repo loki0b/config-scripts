ec206dc1fe3aa5228b33b2300767aa98aa43e184
loki0b <riqc0nt4ct@hotmail.com>
2024-05-12 18:06:20 -0300

    update scripts

--- arch_post_config.sh
+++ arch_post_config.sh
@@ -22,12 +22,19 @@ create_temp_directory () {
   cd "$TMP_DIRECTORY"
 }
 
+config_pacman_conf() {
+    # Enable colors in pacman
+    sudo sed -i 's/^#Color/Color/' "${PACMAN_CONF_PATH}"
+    echo 'Pacman colors enabled'
+}
+
 install_paru () {
   if [[ ! -x $(command -v paru)  ]]; then
   	git clone https://aur.archlinux.org/paru.git
 	cd paru
   	makepkg -si
   	# flip search order
+    sudo sed -i "s/#BottomUp/BottomUp/" /etc/paru.conf
   	paru --gendb
   	paru -c
   	# read about makepkg.conf
@@ -36,6 +43,23 @@ install_paru () {
   	echo 'paru is already installed -- skiping'
   fi
   cd "$TMP_DIRECTORY"
+}
+
+#install_programs () {
+#  # install some programs
+#  sudo pacman -S bitwarden obsidian --needed
+#  echo 'Programs successfully installed'
+#}
+
+install_base_packages () {
+	sudo pacman -S git base-devel --needed --noconfirm
+	echo 'base devel packages installed'
+	
+    sudo pacman -S pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-audio --needed --noconfirm
+	echo 'audio installed'
+	
+    sudo pacman -S xorg-server xorg-xinit --needed --noconfirm
+	echo 'xorg installed'
 }
 
 install_zsh () {
@@ -53,28 +77,24 @@ install_zsh_framework () {
   fi
 }
 
-install_programs () {
-  # install some programs
-  sudo pacman -S bitwarden obsidian --needed
-  echo 'Programs successfully installed'
-}
-
-config_pacman_conf() {
-    # Enable colors in pacman
-    sudo sed -i 's/^#Color/Color/' "${PACMAN_CONF_PATH}"
-    echo 'Pacman colors enabled'
-}
-
 config_zsh_theme () {
     # Switching theme
+    ZSH_THEME_LINE=$(grep -m 1 -n 'ZSH_THEME=' "${ZSHRC_PATH}" | cut -d: -f1)
+    ZSH_DEFAULT_THEME='robbyrussell'
+    ZSH_MY_THEME='gentoo'
+    ZSH_OMZUPDATE_LINE=$(grep -m 3 -n 'zistyle' "$ZSHRC_PATH" | cut -d: -f1)
+    
     sed -i "${ZSH_THEME_LINE}s/${ZSH_DEFAULT_THEME}/${ZSH_MY_THEME}/" ${ZSHRC_PATH}
     echo "New ZSH_THEME ${ZSH_MY_THEME}"
     # Mode reminder omz:update
-    sed -i "${ZSH_OMZUPDATE_LINE}s/# zstyle/zstyle/" "$ZSHRC_PATH"
-    echo 'OMZ remider update mode enabled'
+    #sed -i "${ZSH_OMZUPDATE_LINE}s/# zstyle/zstyle/" "$ZSHRC_PATH"
+    #echo 'OMZ remider update mode enabled'
 }
 
 install_zsh_syntax_highlighting() {
+    ZSH_PLUGIN_LINE=$(grep -m 1 -n '^[^#]*plugins=' "${ZSHRC_PATH}" | cut -d: -f1)
+    PLUGINS="git zsh-syntax-highlighting"
+
 	if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"   ]]; then    
 		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 		sed -i "${ZSH_PLUGIN_LINE}s/git/${PLUGINS}/" ${ZSHRC_PATH}
@@ -84,26 +104,10 @@ install_zsh_syntax_highlighting() {
 	fi
 }
 
-install_base_packages () {
-	sudo pacman -S git base-devel --needed --noconfirm
-	echo 'base packages installed'
-	
-    sudo pacman -S pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-audio --needed --noconfirm
-	echo 'audio installed'
-	
-    sudo pacman -S i3 xorg-server xorg-xinit --needed --noconfirm
-	echo 'i3-group installed'
-}
 
 config_zsh () {
     ZSHRC_PATH="$HOME/.zshrc"
-    ZSH_THEME_LINE=$(grep -m 1 -n 'ZSH_THEME=' "${ZSHRC_PATH}" | cut -d: -f1)
-    ZSH_PLUGIN_LINE=$(grep -m 1 -n '^[^#]*plugins=' "${ZSHRC_PATH}" | cut -d: -f1)
-    ZSH_OMZUPDATE_LINE=$(grep -m 3 -n 'zistyle' "$ZSHRC_PATH" | cut -d: -f1)
-    ZSH_DEFAULT_THEME='robbyrussell'
-    ZSH_MY_THEME='gentoo'
-    PLUGINS="git zsh-syntax-highlighting"
-    
+       
     install_zsh
     config_zsh_theme 
     install_zsh_framework #omz
@@ -115,4 +119,3 @@ config_zsh () {
 #create_temp_directory
 #install_paru
 #config_zsh
-#install_programs
--- vpn-cin.sh
+++ vpn-cin.sh
@@ -13,22 +13,22 @@ nmcli connection add con-name "$name" type vpn vpn-type l2tp autoconnect no
 
 nmcli connection modify "$name" \
 	vpn.data "gateway=vpn.cin.ufpe.br,
-        	  ipsec-enabled=yes,
-        	  ipsec-esp=$esp,
-		  ipsec-ike=$ike,
-        	  machine-auth-type=psk,
-        	  mru=1400,
-		  mtu=1400,
-        	  password-flags=2,
-        	  refuse-chap=yes,
-		  refuse-eap=yes,
-        	  refuse-pap=yes,
-        	  user=$1,
-        	  user-auth-type=password,
-		  service-type=org.freedesktop.NetworkManager.l2tp" \
+             ipsec-enabled=yes,
+        	 ipsec-esp=$esp,
+		     ipsec-ike=$ike,
+        	 machine-auth-type=psk,
+        	 mru=1400,
+		     mtu=1400,
+        	 password-flags=2,
+        	 refuse-chap=yes,
+		     refuse-eap=yes,
+        	 refuse-pap=yes,
+        	 user=$1,
+        	 user-auth-type=password,
+		     service-type=org.freedesktop.NetworkManager.l2tp" \
 	vpn.secrets "ipsec-psk=$psk" \
 	ipv4.method "auto" \
 	ipv6.addr-gen-mode "default" \
-	ipv6.method "auto" \
+	ipv6.method "auto"
 
 echo "CIn VPN Configured"
